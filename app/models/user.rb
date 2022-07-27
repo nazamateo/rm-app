baf4e5a8da976d4c6a5f4d16f572375c9aa1f524
nazamateo <nazamateo@gmail.com>
2022-07-27 20:38:03 +0800

    Add roles to users thru devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: [:admin, :requestor]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :requestor
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
