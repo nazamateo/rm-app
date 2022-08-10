3d1de69356343281448d3c8e3904364029f8e14c
nazamateo <nazamateo@gmail.com>
2022-08-10 20:57:45 +0800

    add rspec

---
+++ spec/system/log_ins_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Log In", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+    let(:admin) { create(:admin) }
+    context "with valid credentials" do
+    it "allows admin to sign in" do
+      visit(new_user_session_path)
+      fill_in "user_email", with: admin.email
+      fill_in "user_password", with: admin.password
+      within("form") { click_on("Log in") }
+
+      expect(page).to have_current_path(traders_path)
+    end
+  end
+end
