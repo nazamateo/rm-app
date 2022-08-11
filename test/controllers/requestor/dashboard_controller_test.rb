efc0808508507a02661fc9b8188b51f32c7988e2
nazamateo <nazamateo@gmail.com>
2022-08-11 23:55:35 +0800

    Delete unneccessary files

--- test/controllers/requestor/dashboard_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Requestor::DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/requestor/jobs_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Requestor::JobsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
