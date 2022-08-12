01773b4c3b430d80ab56fe014590992b4f20cff2
nazamateo <nazamateo@gmail.com>
2022-08-13 02:41:14 +0800

    fix pdf template view

--- app/controllers/admin/pdf_controller.rb
+++ app/controllers/admin/pdf_controller.rb
@@ -1,5 +1,7 @@
 class Admin::PdfController < ApplicationController
-
+    before_action :authenticate_user!
+    before_action :authenticate_admin
+    
     def new
         @job = Job.all.find (params[:job_id])
         @requestor = User.all.find_by(id: @job.user_id)
--- app/wrapper/pdf/api/client.rb
+++ app/wrapper/pdf/api/client.rb
@@ -3,8 +3,9 @@ module Pdf
     	class Client
             BASE_URL = 'http://api.pdflayer.com/api/convert'.freeze
            
-            def get_pdf
-                send_request(:get, 'advice')
+            def get_pdf(docname)
+                send_request(:get, :access_key => '38dd1779e1d0f2299d3669eb967192aa',
+                :document_url => request.original_url, :document_name => docname, :auth_user => 'nrmateo@dlshsi.edu.ph', :auth_pass => 'abc123' )
             end
            
             def send_request(http_method, path_to_resource, params={})
