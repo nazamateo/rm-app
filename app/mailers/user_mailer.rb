2bdd23c6cd52ad740eca3091e90c8ce650bea571
nazamateo <nazamateo@gmail.com>
2022-08-13 08:20:16 +0800

    fix url in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,20 +3,21 @@ class UserMailer < ApplicationMailer
     def job_created_email_noti
         @user = params[:user]
         @job = @user.jobs.last
-        @url  = "http://localhost:3000/requestor/jobs/#{@job.id}"
+        @url  = "https://dlshsi-engg-jo-app.herokuapp.com/requestor/jobs/#{@job.id}"
         mail(to: @user.email, subject: "Job order number #{@job.id} created successfully!")
     end
-
+    
     def job_update_email_noti
         @job = params[:job]
         @user = User.all.find_by(id: @job.user_id)
+        @url  = "https://dlshsi-engg-jo-app.herokuapp.com/requestor/jobs/#{@job.id}"
         mail(to: @user.email, subject: "Job order number #{@job.id} assigned personnel.")
     end
     
     def job_closed_email_noti
         @job = params[:job]
         @user = User.all.find_by(id: @job.user_id)
-        @url  = "http://localhost:3000/requestor/jobs/#{@job.id}/evaluation/new"
+        @url  = "https://dlshsi-engg-jo-app.herokuapp.com/requestor/jobs/#{@job.id}/evaluation/new"
         mail(to: @user.email, subject: "Job order number #{@job.id} closed.")
     end
 
